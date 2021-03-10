af6c0433d5e8da63b0feab8f9d2c9e4b8395fb96
Aaron <isaac@gmail.com>
2021-03-10 12:57:40 -0500

    initial commit.

---
+++ Git/src/main/java/za/ac/myccput/Gittest/Gittest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.myccput.Gittest;
+
+/**
+ *
+ * @author Isaac kissimba
+ */
+public class Gittest {
+    public static int sum(int num1, int num2){
+        return num1+num2;
+    }
+    
+}
